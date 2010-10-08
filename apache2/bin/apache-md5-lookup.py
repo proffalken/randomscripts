339e301b17a82be6bc4f292a465357d6b0d650b5
Prof Falken <theprofessor@threedrunkensysadsonthe.net>
2010-10-08 13:51:21 +0100

    initial import

---
+++ apache2/bin/apache-md5-lookup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# import the relevant libraries
+import sys
+import hashlib
+
+while True:
+    request = sys.stdin.readline().strip()
+    response = hashlib.md5(request).hexdigest()[:4]
+    if response:
+        sys.stdout.write(response + '\n')
+    else:
+        sys.stdout.write('NULL\n')
+    sys.stdout.flush()
+	
