7bcc953b22b3a715078a39a70185ae771e8ecaba
Prof Falken <theprofessor@threedrunkensysadsonthe.net>
2010-10-25 17:16:04 +0200

    added comments to demo file

--- puppet/pyfacter_demo.py
+++ puppet/pyfacter_demo.py
@@ -1,8 +1,14 @@
 #!/usr/bin/env python
+
+
+# import the libraries
 from pyfacter import pyfacter
 
+# initalise pyfacter
 init_fact = pyfacter()
 
+#get the facts in a HASH
 fact_hash = init_fact.get_facts()
 
+# print a given fact to the screen
 print fact_hash['lsbdistdescription']
